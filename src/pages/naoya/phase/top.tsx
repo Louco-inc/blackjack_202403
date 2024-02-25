dae59f4abb24901a50733485697d68bd4e39a63b
mura-work <murata.naoya.work@gmail.com>
2024-02-26 00:32:42 +0900

    [feat] トップ画面のポイントの表示を修正

--- src/pages/naoya/phase/top.tsx
+++ src/pages/naoya/phase/top.tsx
@@ -1,4 +1,5 @@
 import { Text, Button } from "@chakra-ui/react";
+import Image from "next/image";
 
 type PropsType = {
   playerPoint: number;
@@ -11,9 +12,18 @@ export default function TopComponent(props: PropsType): JSX.Element {
   const { playerPoint, onStart, openHowToPlay, openHistory } = props;
   return (
     <>
-      <div className="">
-        <div className="text-end">
-          ポイント数: {playerPoint >= 0 ? playerPoint : "ー"}P
+      <div className="flex justify-end pt-3">
+        <div className="flex items-center bg-white rounded-2xl px-4">
+          <Image
+            className="inline-block"
+            src={"/images/point-icon.png"}
+            alt="ポイントアイコン"
+            width={48}
+            height={48}
+          />
+          <div className="inline-block text-main-color font-bold text-4xl leading-9 pl-3">
+            {playerPoint >= 0 ? playerPoint : "ー"}P
+          </div>
         </div>
       </div>
       <Text fontSize="6xl" className="font-bold text-center mt-32 mb-16">
