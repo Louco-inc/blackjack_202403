de3404cf8745dfa10c16228943a793f0b6bb0bb6
MoriShun89 <s.moririn0724@gmail.com>
2024-02-29 23:31:44 +0900

    [feat]画面配置設定

--- src/pages/morishun/phase/game.tsx
+++ src/pages/morishun/phase/game.tsx
@@ -1,4 +1,9 @@
 import { PlayerType } from "@/types";
+import { 
+  Button,
+  HStack,
+  Text,
+} from '@chakra-ui/react'
 
 type PropsType = {
   playerData: PlayerType;
@@ -12,7 +17,53 @@ type PropsType = {
 export default function GameComponent(props: PropsType): JSX.Element {
   return (
     <>
-      <div className="h-full relative text-white">ゲーム画面</div>
+      <div className="h-full relative text-white">
+        <div className="absolute top-0 right-0">
+          <div>ポイント</div>
+        </div>
+        <div className="grid justify-items-center h-2/6">
+          <div>ディーラー</div>
+        </div>
+        <div className="grid justify-items-center h-3/6">
+          <div>手札</div>
+        </div>
+        <div className="grid justify-items-center h-1/6">
+          <HStack className="w-4/5">
+            <Button 
+              className="w-1/4 mx-1"
+              colorScheme='white' 
+              variant='outline'
+              size="lg"
+            >
+              <Text>ヒット</Text>
+            </Button>
+            <Button 
+              className="w-1/4 mx-1"
+              colorScheme='white' 
+              variant='outline'
+              size="lg"
+            >
+              <Text>ダブル</Text>
+            </Button>
+            <Button 
+              className="w-1/4 mx-1"
+              colorScheme='white' 
+              variant='outline'
+              size="lg"
+            >
+              <Text>スタンド</Text>
+            </Button>
+            <Button 
+              className="w-1/4 mx-1"
+              colorScheme='white' 
+              variant='outline'
+              size="lg"
+            >
+              <Text>サレンダー</Text>
+            </Button>
+          </HStack>
+        </div>
+      </div>
     </>
   );
 }
